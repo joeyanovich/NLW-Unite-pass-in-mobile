316f6752609198fd4bb6705d5aa3ee8d3f25435d
joeyanovich <joedison.dias@gmail.com>
2024-04-11 23:31:46 -0300

    feat ✨: create input component

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,13 +2,16 @@ import "../styles/global.css"
 
 import { Slot } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+
+import { Loading } from "../components/loading"
+
 import {
   useFonts,
   Roboto_700Bold,
   Roboto_500Medium,
   Roboto_400Regular,
 } from "@expo-google-fonts/roboto"
-import { Loading } from "../components/loading"
+
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
