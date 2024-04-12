fd30aea014d5bc9ac63b0c216c873c7f6b212123
joeyanovich <joedison.dias@gmail.com>
2024-04-11 23:01:36 -0300

    font ✏️: install font

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,11 +2,27 @@ import "../styles/global.css"
 
 import { Slot } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_500Medium,
+  Roboto_400Regular,
+} from "@expo-google-fonts/roboto"
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_500Medium,
+    Roboto_400Regular,
+  })
+
+  if (!fontsLoaded) {
+    return
+  }
+
   return (
     <>
-      <StatusBar style="light"/>
+      <StatusBar style="light" />
       <Slot />
     </>
 )
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native'
 export default function Home() {
   return (
     <View className='flex-1 bg-green-500 items-center justify-center'>
-      <Text>Hello React Native</Text>
+      <Text className='text-white text-2xl'>Hello React Native</Text>
     </View>
   )
 }
