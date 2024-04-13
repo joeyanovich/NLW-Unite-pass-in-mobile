dfb7c4340055b0dd6712a4a54af2ee3c6c8b7176
joeyanovich <joedison.dias@gmail.com>
2024-04-12 21:44:03 -0300

    feat ✨: create ticket page

---
+++ src/app/ticket.tsx
@@ -0,0 +1,8 @@
+import { StatusBar, View } from "react-native";
+
+export default function Ticket() {
+  <View className="flex-1 bg-green-500">
+    <StatusBar barStyle="light-content" />
+    
+  </View>
+}
