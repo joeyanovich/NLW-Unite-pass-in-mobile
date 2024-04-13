5a89933431a310f92cc80f7f70ba3897a0a578ac
joeyanovich <joedison.dias@gmail.com>
2024-04-12 22:38:38 -0300

    feat ✨: create credential component

--- src/app/ticket.tsx
+++ src/app/ticket.tsx
@@ -1,11 +1,13 @@
 import { StatusBar, View } from "react-native";
 import { Header } from "../components/header";
+import { Credential } from "../components/credential";
 
 export default function Ticket() {
   return (
     <View className="flex-1 bg-green-500">
       <StatusBar barStyle="light-content" />
       <Header title="Minha Credencial"/>
+      <Credential />
     </View>
   )
 }
