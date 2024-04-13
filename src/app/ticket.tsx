8270836b0b9755d8c036a225a32b6020072d77a5
joeyanovich <joedison.dias@gmail.com>
2024-04-12 21:59:37 -0300

    feat ✨: create header component

--- src/app/ticket.tsx
+++ src/app/ticket.tsx
@@ -1,8 +1,11 @@
 import { StatusBar, View } from "react-native";
+import { Header } from "../components/header";
 
 export default function Ticket() {
-  <View className="flex-1 bg-green-500">
-    <StatusBar barStyle="light-content" />
-    
-  </View>
+  return (
+    <View className="flex-1 bg-green-500">
+      <StatusBar barStyle="light-content" />
+      <Header title="Minha Credencial"/>
+    </View>
+  )
 }
